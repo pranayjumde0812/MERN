fe0db383f6a992745c5cfd96415ea032972ef989
Pranay Jumde <pranay.jumde@qodequay.com>
2024-10-23 12:26:47 +0530

    all users routes done

--- MERN-AUTH/backend/controllers/userController.js
+++ MERN-AUTH/backend/controllers/userController.js
@@ -2,11 +2,43 @@ import asyncHandler from "express-async-handler";
 
 // @desc    Auth user/set token
 // route    POST /api/users/auth
-// access   Public
+// @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(401);
-  throw new Error("Something went wrong");
   res.status(200).json({ message: "Auth User" });
 });
 
-export { authUser };
+// @desc    Register a new user
+// route    POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+// @desc    Logout the user
+// route    POST /api/users/logout
+// @access  Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// @desc    Get user profile
+// route    GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User profile" });
+});
+
+// @desc    Get user profile
+// route    PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User profile updated" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
