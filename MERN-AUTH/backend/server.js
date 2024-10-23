9fa2b2cf7a1a177fdfd33e2e349b345485b427d8
Pranay Jumde <pranay.jumde@qodequay.com>
2024-10-23 12:04:21 +0530

    Error handler added

--- MERN-AUTH/backend/server.js
+++ MERN-AUTH/backend/server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 6000;
 const app = express();
 
-
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send("Server is ready"));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server has started on port ${port}`));
