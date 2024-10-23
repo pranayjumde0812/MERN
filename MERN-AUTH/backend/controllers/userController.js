8e7136dcff3f52112ce481c2d5244703ca385994
Pranay Jumde <pranay.jumde@qodequay.com>
2024-10-23 15:19:51 +0530

    Logout And destroy cookie

--- MERN-AUTH/backend/controllers/userController.js
+++ MERN-AUTH/backend/controllers/userController.js
@@ -54,6 +54,10 @@ const registerUser = asyncHandler(async (req, res) => {
 // route    POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
   res.status(200).json({ message: "Logout User" });
 });
 
